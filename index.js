4c8357b055bf4591a5383420bb031a4dc36a5511
Snir Evgi <snirevg@gmail.com>
2023-03-01 21:27:27 +0200

    multipuleChanges

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ function newFn() {
   return 2 + 1;
 }
 console.log("blah blah");
+console.log("hello world");
+function newFn() {
+  return 2 + 1;
+}
+console.log("blah blah");
+console.log("hello world");
+function newFn() {
+  return 2 + 1;
+}
+console.log("blah blah");
+console.log("hello world");
+function newFn() {
+  return 2 + 1;
+}
+console.log("blah blah");
+console.log("hello world");
+function newFn() {
+  return 2 + 1;
+}
+console.log("blah blah");
+console.log("hello world");
+function newFn() {
+  return 2 + 1;
+}
+console.log("blah blah");
